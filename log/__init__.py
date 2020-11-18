07cd99f89d3cf0af59c82881d1bdad1302370bfe
Dotan Cohen <github-com-2010.04@dotancohen.com>
2020-11-18 02:54:57 +0200

    Support most arbitrary git log flags

--- log/__init__.py
+++ log/__init__.py
@@ -5,12 +5,13 @@ import subprocess
 
 
 def main(args):
-	process_args = ['git', 'log']
+	process_args = ['git', 'log'] + args.last
 	subprocess.run(process_args)
 	return
 
 
 def add_subparser(subparsers):
 	parser = subparsers.add_parser('log')
+	parser.add_argument('last', nargs='*')
 	return
 
